8fc8731011164d778f10f4d04943a9577b490673
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-09-02 03:14:17 +0600

    console removed

--- js/book-archive.js
+++ js/book-archive.js
@@ -1,40 +1,40 @@
-// toggler 
-const toggleSearch = displayStyle => {
-    document.getElementById('searched').style.display = displayStyle;
-}
+// toggler
+const toggleSearch = (displayStyle) => {
+  document.getElementById("searched").style.display = displayStyle;
+};
 
-// get and decode the url 
+// get and decode the url
 const getBooks = async () => {
-    const input = document.getElementById('search-input');
-    const value = input.value;
+  const input = document.getElementById("search-input");
+  const value = input.value;
 
-    const url = `https://openlibrary.org/search.json?q=${value}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    input.value = '';
+  const url = `https://openlibrary.org/search.json?q=${value}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  input.value = "";
 
-    if (data.docs.length === 0) {
-        document.getElementById("total-books").innerText = 'No';
-    } else {
-        document.getElementById("total-books").innerText = data.numFound;
-    }
-    
-    toggleSearch("block");
-    console.log(data);
-    displayBooks(data.docs);
-}
+    // checking length 
+  if (data.docs.length === 0) {
+    document.getElementById("total-books").innerText = "No";
+  } else {
+    document.getElementById("total-books").innerText = data.numFound;
+  }
+
+  toggleSearch("block");
+  displayBooks(data.docs);
+};
 
 // display items to booksContainer
-const displayBooks = books => {
-    const booksContainer = document.getElementById('books-container');
-    booksContainer.textContent = '';
-    books.forEach(book => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        const url = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
-        div.innerHTML = `
+const displayBooks = (books) => {
+  const booksContainer = document.getElementById("books-container");
+  booksContainer.textContent = "";
+  books.forEach((book) => {
+    const div = document.createElement("div");
+    div.classList.add("col");
+    const url = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+    div.innerHTML = `
                     <div class="card">
-                        <img src="${url}?${url}" class="card-img-top" alt="...">
+                        <img src="${url}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${book.title}</h5>
                             <p class="card-text">Author : ${book?.author_name}</p>
@@ -42,6 +42,6 @@ const displayBooks = books => {
                         </div>
                     </div>
         `;
-        booksContainer.appendChild(div);
-    })
-}
+    booksContainer.appendChild(div);
+  });
+};
