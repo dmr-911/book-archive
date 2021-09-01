d9ebf0b0c172e847a848db332880925e37311fa8
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-09-02 00:18:58 +0600

    completed without no result found

---
+++ js/book-archive.js
@@ -0,0 +1,36 @@
+const toggleSearch = displayStyle => {
+    document.getElementById('searched').style.display = displayStyle;
+}
+const getBooks = async () => {
+    const input = document.getElementById('search-input');
+    const value = input.value;
+
+    const url = `http://openlibrary.org/search.json?q=${value}`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    input.value = '';
+    document.getElementById("total-books").innerText = data.docs.length;
+    toggleSearch("block");
+    displayBooks(data.docs)
+}
+const displayBooks = books => {
+    const booksContainer = document.getElementById('books-container');
+    booksContainer.textContent = '';
+    books.forEach(book => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        const url = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+        div.innerHTML = `
+                    <div class="card">
+                        <img src="${url}? ${url} : 'No Image found'" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${book.title}</h5>
+                            <p class="card-text">Author : ${book?.author_name}</p>
+                            <p class="card-text">First Published in : ${book?.first_publish_year}</p>
+                        </div>
+                    </div>
+        `;
+        booksContainer.appendChild(div);
+    })
+}
