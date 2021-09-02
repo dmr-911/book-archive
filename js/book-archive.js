e0e375a5d48de6dce41745961df83bd285531c44
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-09-02 13:32:42 +0600

    card background updated

--- js/book-archive.js
+++ js/book-archive.js
@@ -55,7 +55,7 @@ const displayBooks = (books) => {
     div.innerHTML = `
                     <div class="card border border-2 border-dark">
                         <img src="${url}" class="card-img-top" alt="image of a reading book">
-                        <div class="card-body bg-info">
+                        <div class="card-body bg-success text-white">
                             <h5 class="card-title" title="${book.title}">${book.title ? book.title.slice(0, 14) : "Unknown Name"}</h5>
                             <p class="author card-text" title="${book.author_name}">Author : ${book.author_name ?  book.author_name[0].slice(0, 14) : "Unknown Author"}</p>
                             <p class="card-text" title="${book.publish_date}">First Published : ${book.publish_date
